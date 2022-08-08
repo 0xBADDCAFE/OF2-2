fe3acea32d4b6b3863f974aa41f9dc7c5b3a9ef3
0xBADDCAFE <0xbdcf@gmail.com>
2022-08-09 06:24:32 +0900

    vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      // OR
+      // injectManifest: {
+      //   globPatterns: [],
+      // },
+      devOptions: { enabled: true },
+    }),
+  ],
 });
